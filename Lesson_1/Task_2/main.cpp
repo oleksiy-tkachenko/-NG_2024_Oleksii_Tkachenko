f8ba7b0f47f3f866030f22f2ce22d5fe90436765
Sans91 <sansomailjokes@gmail.com>
2024-06-06 21:28:47 +0300

    Made L_1/T_2. Very simple calculator which adds two inputted numbers

---
+++ Lesson_1/Task_2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a,b;
+    cout << "Calculator\n\nEnter two numbers to add, separating them either by pressing enter or spacebar\n";
+    cin >> a >> b;
+    cout << a+b << endl;
+    system("pause");
+    return 0;
+}

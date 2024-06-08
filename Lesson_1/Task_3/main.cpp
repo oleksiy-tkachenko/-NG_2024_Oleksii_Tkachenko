7c30172c5cd55e9c91cdd89de473b8347d75fec9
Sans91 <sansomailjokes@gmail.com>
2024-06-08 08:20:09 +0300

    Made L_1/T_3. Program asks for income number, then outputs some answer string depending on it

---
+++ Lesson_1/Task_3/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int income;
+    cout << "How much money do you make?" << endl;
+    cin >> income;
+    if(income < 1000){
+        cout << "You need to work harder.";
+    }
+    if(income > 1000){
+        if (income > 1000000){
+            cout << " Wow, you are a millionaire!";
+        }
+        if(income < 100000){
+            cout << "Keep up the great work!";
+        }
+    }
+    cout << " And you are awesome!\n";
+    system("pause");
+}

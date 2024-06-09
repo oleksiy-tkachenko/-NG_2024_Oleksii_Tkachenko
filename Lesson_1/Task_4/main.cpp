b58c1449fb2cd823eb56a8cf7a6bf4f504f8e35d
Sans91 <sansomailjokes@gmail.com>
2024-06-09 20:53:19 +0300

    Fixed L_1/T_4. Renamed a b variables to number1 number2

--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -6,39 +6,39 @@ using namespace std;
 
 int main()
 {
-    double a, b, result;
+    double number1, number2, result;
     int operation;
     bool isSuccessful = true;
 
     cout << "Calculator\n\nEnter two numbers to do math operations with, separating them either by pressing enter or spacebar\n";
-    cin >> a >> b;
+    cin >> number1 >> number2;
     cout << "\nChoose the math operation(enter the number of available operation)\n"
             "List of available operations\n0. addition\n1. subtraction\n2. multiplication\n"
             "3. division\n4. square(using first)\n5. squareroot(using first)\n";
     cin >> operation;
     switch(operation){
         case(0): // addition
-            result = a + b;
+            result = number1 + number2;
             break;
         case(1): // subtraction
-            result = a - b;
+            result = number1 - number2;
             break;
         case(2): // multiplication
-            result = a * b;
+            result = number1 * number2;
             break;
         case(3): // division
-            if(b == 0){
+            if(number2 == 0){
                 cout << "Infinity\n";
                 isSuccessful = false;
                 break;
             }
-            result = a / b;
+            result = number1 / number2;
             break;
         case(4): // square of first number
-            result = a * a;
+            result = number1 * number1;
             break;
         case(5): // square root of first number
-            result = sqrt(a);
+            result = sqrt(number1);
             break;
         default: // invalid number
             cout << "No such operation!\n";

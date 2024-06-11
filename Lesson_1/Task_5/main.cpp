5103131d56e918113cd067503fba32d1ce4d59ac
Sans91 <sansomailjokes@gmail.com>
2024-06-11 22:33:22 +0300

    Fixed L_1/T_5. Made code smaller, removed unnessecary parts, removed include <ccomplex>

--- Lesson_1/Task_5/main.cpp
+++ Lesson_1/Task_5/main.cpp
@@ -1,38 +1,29 @@
 #include <iostream>
 #include <cmath>
-#include <ccomplex>
 
 using namespace std;
 
 int main()
 {
-    double a, b, c, discriminant, root1, root2, realPart, realDiscriminantPart, imaginaryDiscriminantPart;
+    double a, b, c;
     cout << "Input a, b and c of quadratic equation, separating them either by pressing enter or spacebar:" << endl;
     cin >> a >> b >> c;
-    discriminant = b*b - 4*a*c;
-    realPart = -b/(2*a);
+
+    double discriminant = b*b - 4*a*c;
+    double realPart = -b/(2*a);
     if(discriminant > 0) {
         cout << "Discriminant is higher than 0, so\n";
-        realDiscriminantPart = sqrt(discriminant / (2*a));
-        root1 = realPart + realDiscriminantPart;
-        root2 = realPart - realDiscriminantPart;
-        cout << "First root = " << root1 << endl
-             << "Second root = " << root2 << endl;
+        double realDiscriminantPart = sqrt(discriminant / (2*a));
+        cout << "First root = " << realPart + realDiscriminantPart << endl
+             << "Second root = " << realPart - realDiscriminantPart << endl;
     } else if (discriminant == 0) {
-        cout << "Discriminant is 0, so\n";
-        root1 = realPart;
-        cout << "Only root = " << root1 << endl;
+        cout << "Discriminant is 0, so\n"
+             << "Only root = " << realPart << endl;
     } else {
         cout << "Discriminant is less than 0, so\n";
-        imaginaryDiscriminantPart = sqrt(-discriminant)/(2*a);
-        complex<double> root1;
-        root1.real(realPart);
-        root1.imag(imaginaryDiscriminantPart);
-        complex<double> root2;
-        root2.real(realPart);
-        root2.imag(-imaginaryDiscriminantPart);
-        cout << "Fisrt root = " << root1.real() << showpos << root1.imag() << "i"  << endl;
-        cout << "Secound root = " << root2.real() << showpos << root2.imag() << "i"  << endl;
+        double imaginaryDiscriminantPart = sqrt(-discriminant)/(2*a);
+        cout << "Fisrt root = " << realPart << showpos << imaginaryDiscriminantPart<< "i" << endl;
+        cout << "Secound root = " <<  realPart << showpos << -imaginaryDiscriminantPart << "i"  << endl;
     }
     system("pause");
 }

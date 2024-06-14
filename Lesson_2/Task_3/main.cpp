a36c7681eedadbca91b7b9bb30444515447d44da
Sans91 <sansomailjokes@gmail.com>
2024-06-14 11:07:23 +0300

    Made L_2/T_3. Program which asks for numbers as input(max 5 numbers), and ouputs * in columns

---
+++ Lesson_2/Task_3/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int columns[5];
+    int maxNumber = 0;
+    for (int column = 0; column < 5; column++) {
+        cout << "Enter amount in column " << column << ": ";
+        cin >> columns[column];
+        if(columns[column] > maxNumber){
+            maxNumber = columns[column];
+        }
+    }
+
+    for (int row = 0; row < maxNumber; row++) {
+        for (int column = 0; column < 5; column++) {
+            if(columns[column] > row) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    system("pause");
+}

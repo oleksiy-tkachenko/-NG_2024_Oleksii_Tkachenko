bf0fada4057cc0cf036491068300f7c3e92dd4d0
Sans91 <sansomailjokes@gmail.com>
2024-06-27 19:36:17 +0300

    Fixed L_2/T_2. Renamed index iterable to column for more clarity, added startPoint and endPoint variables for more readability, fixed small stylistic errors.

--- Lesson_2/Task_2/main.cpp
+++ Lesson_2/Task_2/main.cpp
@@ -4,14 +4,15 @@ using namespace std;
 
 int main()
 {
+    // i_ means iterable
     int sizes[20];
-    int maxSize = 0;
+    int maxSizeValue = 0;
     int stopIndex = 19;
-    for(int i_row = 0; i_row<20; i_row++) {
+    for(int i_row = 0; i_row < 20; i_row++) {
         cout << "Enter size of the section(enter only odd numbers, 0 to stop): ";
         cin >> sizes[i_row];
-        if(sizes[i_row] > maxSize) {
-            maxSize = sizes[i_row];
+        if(sizes[i_row] > maxSizeValue) {
+            maxSizeValue = sizes[i_row];
         }
         if(sizes[i_row] % 2 == 0) {
             stopIndex = i_row;
@@ -20,10 +21,12 @@ int main()
     }
 
     for (int i_row = 0; i_row < stopIndex; i_row++) {
-        for (int i_index = 0; i_index < (maxSize - sizes[i_row]) / 2.0; i_index++) {
+        int startPoint =  (maxSizeValue - sizes[i_row]) / 2.0;
+        int endPoint = startPoint + sizes[i_row];
+        for (int i_column = 0; i_column < startPoint; i_column++) {
             cout << " ";
         }
-        for (int i_index = (maxSize - sizes[i_row]) / 2.0; i_index < (maxSize - sizes[i_row]) / 2.0 + sizes[i_row]; i_index++) {
+        for (int i_column = startPoint; i_column < endPoint; i_column++) {
             cout << "*";
         }
         cout << endl;
